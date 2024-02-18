bbd01fd11a5dc1ac69d46f0355b7d7e5ba39ce20
IssahBen <benedictissah@gmail.com>
2024-02-18 05:01:46 -0600

    init

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,4 +1,2 @@
-$redis = Redis.new({
-  url: ENV.fetch("REDIS_URL", "redis://localhost:6379"),
-  ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE }
-})
+
+$redis = Redis.new(url: ENV["redis://:pcade59066a193168b54132ce927691b95898b03dd5b0f1db09f9c6bf3b48671b@ec2-34-206-166-87.compute-1.amazonaws.com:8399"], ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE })
