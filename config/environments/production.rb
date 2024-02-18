8e83a86dd8e76b696ea52c695310fdfe91cfaedf
IssahBen <benedictissah@gmail.com>
2024-02-18 02:17:00 -0600

    init

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -95,4 +95,5 @@ Rails.application.configure do
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
   config.web_socket_url = "wss://shrouded-depths-23189.herokuapp.com/cable"
+  config.action_cable.allowed_request_origins = ['https://shrouded-depthd-23189.herokuapp.com', 'http://shrouded-depthd-23189.herokuapp.com']
 end
