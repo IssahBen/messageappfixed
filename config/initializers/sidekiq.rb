19241d0c37f9d6f31ad5b525eb0c972e3d3f936e
IssahBen <benedictissah@gmail.com>
2024-02-18 03:21:24 -0600

    init

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,13 +1,13 @@
 Sidekiq.configure_server do |config|
     config.redis = {
-      url: ENV["REDIS_URL"],
+      url: ENV["redis://:pcade59066a193168b54132ce927691b95898b03dd5b0f1db09f9c6bf3b48671b@ec2-34-206-166-87.compute-1.amazonaws.com:8399"],
       ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE }
     }
   end
   
   Sidekiq.configure_client do |config|
     config.redis = {
-        url: ENV["REDIS_URL"],
+        url: ENV["redis://:pcade59066a193168b54132ce927691b95898b03dd5b0f1db09f9c6bf3b48671b@ec2-34-206-166-87.compute-1.amazonaws.com:8399"],
         ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE }
     }
   end
